12b2c3ac60ab1c24212a53e754a11bcf003c8675
Vikas Kumar Saw <vikasvks0112@gmail.com>
2020-10-22 13:37:08 +0530

    registered main and sidebar components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <main>
-    <section id="main"></section>
-    <section id="side"></section>
+    <section id="main"><app-main /></section>
+    <section id="side"><app-side /></section>
   </main>
   <section id="nav"></section>
 </template>
 
 <script>
+import sidebar from './components/sidebar'
+import main from './components/main'
 export default {
-
+  components:{
+    'app-side' : sidebar,
+    'app-main': main,
+  }
 }
 </script>
 
